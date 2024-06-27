7cc207418181b0ceea71e7d55d69d6917f89a3e0
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-06-27 10:38:39 +0530

    added all the components int he app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Model from "./components/Model";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import HowItWorks from "./components/HowItWorks";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Hero />
         <Highlights />
         <Model />
+        <Features/>
+        <HowItWorks/>
+        <Footer/>
       </main>
     </>
   );
